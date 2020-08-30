c515fde874f073a89f92c8cd47b12a4e8d06631b
SandelsCol <62896050+SandelsCol@users.noreply.github.com>
2020-08-30 10:45:41 -0500

    Agregar archivos de proyecto.

---
+++ Page1.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EventParadigmQuiz
+{
+    /// <summary>
+    /// Lógica de interacción para Page1.xaml
+    /// </summary>
+    public partial class Page1 : Page
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void btnlogin_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtusername.Text == "Sandels" && txtpassword.Password == "2122")
+            {
+               MainWindow w = (MainWindow) Window.GetWindow(this);
+                w.MainFrame.NavigationService.Navigate(new Page2());
+            }
+            else
+            {
+                MessageBox.Show("Invalid Credentials");
+            }
+        }
+    }
+}
---
+++ Page2.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EventParadigmQuiz
+{
+    /// <summary>
+    /// Lógica de interacción para Page2.xaml
+    /// </summary>
+    public partial class Page2 : Page
+    {
+        public Page2()
+        {
+            InitializeComponent();
+        }
+
+        private void btnsave_Click(object sender, RoutedEventArgs e)
+        {
+            string ProductName = txtproductname.Text;
+            string category = lblcategory.Content.ToString();
+
+            txtresult.Text = ProductName + "\n" + category;
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            RadioButton rb = sender as RadioButton;
+            if (rb.IsChecked.Value)
+            {
+                lblcategory.Content = rb.Content.ToString();
+            }
+        }
+    }
+}
